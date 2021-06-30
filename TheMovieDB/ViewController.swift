73c3778ae737a1ce62cc241e083ab31741b30872
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-06-30 20:20:49 -0300

    Added initial viewController configuration

--- TheMovieDB/ViewController.swift
+++ TheMovieDB/ViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {    
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var popularMovies = [String]()
+    var playingMovies = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieTableViewCell
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return popularMovies.count
+        }
+        
+        return playingMovies.count
+    }
 }
 
